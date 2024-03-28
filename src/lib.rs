6096a8bc16834322b2109efd76337d5b222fcf15
ninokeldishvili <ninokeldishvili@gmail.com>
2024-03-28 13:08:11 +0400

    cbor_encode_addresses

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use hex::FromHex;
 use js_sys::Array;
 use wasm_bindgen::prelude::*;
+use rain_metadata::meta::{
+                             RainMetaDocumentV1Item, ContentType, ContentEncoding, ContentLanguage,
+                             KnownMagic
+                         };
+use serde_wasm_bindgen::to_value;
 
 #[wasm_bindgen]
 pub fn concat_hex_addresses(addresses: Array) -> Vec<u8> {
@@ -17,4 +22,27 @@ pub fn concat_hex_addresses(addresses: Array) -> Vec<u8> {
        }
 
     concatenated
+}
+
+
+#[wasm_bindgen]
+pub async fn cbor_encode_addresses(addresses: Vec<u8>) -> JsValue {
+    let meta_map = RainMetaDocumentV1Item {
+        payload: serde_bytes::ByteBuf::from(addresses),
+        magic: KnownMagic::AddressList,
+        content_type: ContentType::Cbor,
+        content_encoding: ContentEncoding::None,
+        content_language: ContentLanguage::None,
+    };
+
+    match meta_map.cbor_encode() {
+        Ok(cbor_encoded) => {
+            if let Ok(value) = to_value(&cbor_encoded) {
+                value
+            } else {
+                JsValue::from_str("Error converting to JsValue")
+            }
+        },
+        Err(err) => JsValue::from_str(&format!("CBOR encoding error: {:?}", err)),
+    }
 }
