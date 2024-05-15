2ad795f713fb16c971d3597efe41efa77aa12550
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-15 11:22:45 +0400

    Add hex_to_bytes

--- src/lib.rs
+++ src/lib.rs
@@ -22,14 +22,29 @@ pub fn concat_hex_addresses(addresses: Array) -> Vec<u8> {
     concatenated
 }
 
+#[wasm_bindgen]
+pub fn hex_to_bytes(hex_val: &str) -> Option<Vec<u8>> {
+    // Remove the '0x' prefix if present
+    let cleaned_hex = if hex_val.starts_with("0x") {
+        &hex_val[2..]
+    } else {
+        hex_val
+    };
+
+    // Attempt to parse the hex string into bytes
+    match hex::decode(cleaned_hex) {
+        Ok(bytes) => Some(bytes),
+        Err(_) => None,
+    }
+}
 
 #[wasm_bindgen]
-pub async fn cbor_encode_addresses(addresses: Array) -> JsValue {
+pub async fn cbor_encode_addresses(address: Vec<u8> ) -> JsValue {
     // Concatenate the addresses using concat_hex_addresses function
-    let concatenated_addresses = concat_hex_addresses(addresses);
+//     let concatenated_addresses = concat_hex_addresses(addresses);
 
     let meta_map = RainMetaDocumentV1Item {
-        payload: serde_bytes::ByteBuf::from(concatenated_addresses),
+        payload: serde_bytes::ByteBuf::from(address),
         magic: KnownMagic::AddressList,
         content_type: ContentType::Cbor,
         content_encoding: ContentEncoding::None,
